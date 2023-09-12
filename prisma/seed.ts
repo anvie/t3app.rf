ce8cd4c647e0f532076e72b38c783055e1e9603f
anvie <robinsyihab@gmail.com>
2023-09-12 14:52:08 +0700

    add db:sync, db:reset, and seed command

---
+++ prisma/seed.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // @TODO(You): Write your seed code here
+
+  // const alice = await prisma.user.upsert({
+  //   where: { email: 'alice@prisma.io' },
+  //   update: {},
+  //   create: {
+  //     email: 'alice@prisma.io',
+  //     name: 'Alice'
+  //   },
+  // })
+  // const bob = await prisma.user.upsert({
+  //   where: { email: 'bob@prisma.io' },
+  //   update: {},
+  //   create: {
+  //     email: 'bob@prisma.io',
+  //     name: 'Bob'
+  //   },
+  // })
+  // console.log({ alice, bob })
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
