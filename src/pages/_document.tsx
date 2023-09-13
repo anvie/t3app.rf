143f50f96e805f5ec7a5b0a73a8248d4ab5ecbb1
anvie <robinsyihab@gmail.com>
2023-09-13 20:04:32 +0700

    add custom _document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { cn } from '@utils/cssutil';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    const pageProps = this.props?.__NEXT_DATA__?.props?.pageProps;
+    return (
+      <Html>
+        <Head />
+        <body className={cn(pageProps.isDark ? 'dark-mode' : 'light-mode')}>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
