f5e5575d27003d503a38662587a9c33330737810
anvie <robinsyihab@gmail.com>
2024-09-19 13:15:29 +0700

    update schema and

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -19,32 +19,38 @@ export const serverSchema = z.object({
     // VERCEL_URL doesn't include `https` so it cant be validated as a URL
     process.env.VERCEL ? z.string() : z.string().url()
   ),
-  DISCORD_CLIENT_ID: z.string(),
-  DISCORD_CLIENT_SECRET: z.string(),
+  // DISCORD_CLIENT_ID: z.string(),
+  // DISCORD_CLIENT_SECRET: z.string(),
 
-  // NU.ID config
-  NUID_API_URL: z.string().url(),
-  NUID_CLIENT_ID: z.string(),
-  NUID_CLIENT_SECRET: z.string(),
+  // // NU.ID config
+  // NUID_API_URL: z.string().url(),
+  // NUID_CLIENT_ID: z.string(),
+  // NUID_CLIENT_SECRET: z.string(),
+
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
 });
 
 /**
  * You can't destruct `process.env` as a regular object in the Next.js
  * middleware, so you have to do it manually here.
- * @type \{{ [k in keyof z.infer<typeof serverSchema>]: z.infer<typeof serverSchema>[k] | undefined }}
+ * @type {{ [k in keyof z.infer<typeof serverSchema>]: z.infer<typeof serverSchema>[k] | undefined }}
  */
 export const serverEnv = {
   DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-  DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+  // DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+  // DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
 
-  // NU.ID config
-  NUID_API_URL: process.env.NUID_API_URL,
-  NUID_CLIENT_ID: process.env.NUID_CLIENT_ID,
-  NUID_CLIENT_SECRET: process.env.NUID_CLIENT_SECRET,
+  // // NU.ID config
+  // NUID_API_URL: process.env.NUID_API_URL,
+  // NUID_CLIENT_ID: process.env.NUID_CLIENT_ID,
+  // NUID_CLIENT_SECRET: process.env.NUID_CLIENT_SECRET,
+
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
 };
 
 /**
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -22,13 +22,17 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-    NuSidProvider({
-      clientId: env.NUID_CLIENT_ID,
-      clientSecret: env.NUID_CLIENT_SECRET,
+    // DiscordProvider({
+    //   clientId: env.DISCORD_CLIENT_ID,
+    //   clientSecret: env.DISCORD_CLIENT_SECRET,
+    // }),
+    // NuSidProvider({
+    //   clientId: env.NUID_CLIENT_ID,
+    //   clientSecret: env.NUID_CLIENT_SECRET,
+    // }),
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
     /**
      * ...add more providers here
